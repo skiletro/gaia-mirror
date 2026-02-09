c7ca56462088d46faa67bf59cee691bf7d165d4b
jamie <git@skilet.ro>
2026-02-09 12:27:12 +0000

    chore: clear repo for erebus rewrite

--- secrets/secrets.nix
+++
@@ -1,9 +0,0 @@
-let
-  keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINnFEMa0S9zuA5cVg+Ktazz9gEevkDCNYIDX0WAMxcAC eos"
-  ];
-in {
-  "github-access-token.age".publicKeys = keys;
-  "rclone-protondrive.age".publicKeys = keys;
-  "user-password.age".publicKeys = keys;
-}
