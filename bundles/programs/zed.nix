e45f1c9b9398093a6148a0eea52cd53b4d1663e2
jamie <git@skilet.ro>
2026-02-15 23:16:03 +0000

    feat(zed): add more configuration

--- bundles/programs/zed.nix
+++ bundles/programs/zed.nix
@@ -10,13 +10,27 @@ bundleLib.mkEnableModule [ "gaia" "programs" "zed" ] {
           "nix"
         ];
         userSettings = {
-          helix_mode = true;
-          title_bar.show_sign_in = false;
+          # keep-sorted start block=yes
+          auto_update = false;
+          collaboration_panel.button = false;
+          colorize_brackets = true;
+          disable_ai = true;
           features.edit_prediction_provider = "none";
+          helix_mode = true;
+          load_direnv = "shell_hook";
+          relative_line_numbers = true;
+          restore_on_startup = "none";
+          search.regex = false;
+          tab_bar = {
+            show = false;
+            show_nav_history_buttons = false;
+          };
           telemetry = {
             metrics = false;
             diagnostics = false;
           };
+          title_bar.show_sign_in = false;
+          # keep-sorted end
         };
         extraPackages = with pkgs; [
           nixd
