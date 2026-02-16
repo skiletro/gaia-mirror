f4fe944dab2dd535edbd9797374bf019906fa46a
jamie <git@skilet.ro>
2026-02-16 20:18:04 +0000

    feat(eris): add hardware config and format file

--- hosts/eris.nix
+++ hosts/eris.nix
@@ -1,3 +1,4 @@
+{ inputs, lib, ... }:
 {
   gaia = {
     desktop = "hyprland";
@@ -13,6 +14,7 @@
       discord.enable = true;
       gamemode.enable = true;
       git.enable = true;
+      gsr.enable = true;
       helium.enable = true;
       helix.enable = true;
       kitty.enable = true;
@@ -20,12 +22,62 @@
       nu.enable = true;
       proton.enable = true;
       spotify.enable = true;
-      zed.enable = true;
       steam.enable = true;
       wivrn.enable = true;
-      gsr.enable = true;
+      zed.enable = true;
       # keep-sorted end
     };
     state.system = "25.11";
+  };
+
+  nixos = {
+    imports = with inputs.nixos-hardware.nixosModules; [
+      # keep-sorted start
+      common-cpu-amd
+      common-cpu-amd-pstate
+      common-cpu-amd-zenpower
+      common-gpu-amd
+      common-pc-ssd
+      # keep-sorted end
+    ];
+
+    # Boot Hardware
+    boot.initrd.availableKernelModules = [
+      # keep-sorted start
+      "ahci"
+      "nvme"
+      "sd_mod"
+      "usb_storage"
+      "usbhid"
+      "xhci_pci"
+      # keep-sorted end
+    ];
+    boot.initrd.kernelModules = [ ];
+    boot.kernelModules = [ "kvm-amd" ];
+    boot.extraModulePackages = [ ];
+    boot.supportedFilesystems = [ "ntfs" ];
+
+    # Drives
+    fileSystems."/" = {
+      device = "/dev/disk/by-uuid/94e933e3-aaf9-4cf3-b7c9-044306fce269";
+      fsType = "ext4";
+    };
+
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-uuid/F4AF-09F7";
+      fsType = "vfat";
+      options = [
+        "fmask=0077"
+        "dmask=0077"
+      ];
+    };
+
+    swapDevices = lib.singleton { device = "/dev/disk/by-uuid/ae2dc9bc-8451-48f4-a42b-916e449f30b8"; };
+
+    hardware = {
+      enableRedistributableFirmware = true;
+      amdgpu.overdrive.enable = true;
+    };
+
   };
 }
