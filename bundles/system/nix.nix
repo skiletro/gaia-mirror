c2c41c6d6ebceaf61240b989434e4c611229aced
jamie <git@skilet.ro>
2026-02-17 22:46:59 +0000

    feat(nix): update bundle

--- bundles/system/nix.nix
+++ bundles/system/nix.nix
@@ -2,9 +2,38 @@ let
   nixpkgs.config = {
     allowUnfree = true;
   };
+
+  nix = {
+    enable = true;
+    channel.enable = false;
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      trusted-users = [
+        "jamie"
+        "root"
+        "@wheel"
+      ]; # Fixes some "cannot connect to socket" issues
+      warn-dirty = false;
+      http-connections = 50;
+      log-lines = 50;
+      builders-use-substitutes = true;
+      accept-flake-config = true;
+    };
+
+    optimise = {
+      automatic = true;
+      dates = [
+        "03:45"
+        "07:00"
+      ];
+    };
+  };
 in
 {
-  nixos = { inherit nixpkgs; };
+  nixos = { inherit nix nixpkgs; };
 
-  darwin = { inherit nixpkgs; };
+  darwin = { inherit nix nixpkgs; };
 }
