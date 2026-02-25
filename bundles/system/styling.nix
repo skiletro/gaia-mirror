097d77ddaa30fe048dd2cfeb1ee547d316707da5
jamie <git@skilet.ro>
2026-02-25 00:05:37 +0000

    feat(styling): refactor bundle to apply wallpaper on darwin

--- bundles/system/styling.nix
+++ bundles/system/styling.nix
@@ -6,29 +6,40 @@
   ...
 }:
 let
-  base16Scheme = "${self'.packages.base16-schemes}/share/themes/rose-pine.yaml";
-  polarity = "dark";
-  fonts = pkgs: config: {
-    sansSerif = {
-      package = pkgs.work-sans;
-      name = "Work Sans";
-    };
-    serif = config.stylix.fonts.sansSerif; # Set serif font to the same as the sans-serif
-    monospace = {
-      package = self'.packages.liga-sf-mono-nerd-font;
-      name = "Liga SFMono Nerd Font";
-    };
-    emoji = {
-      package = self'.packages.apple-emoji;
-      name = "Apple Color Emoji";
-    };
+  sharedStylixConfig = config: pkgs: {
+    base16Scheme = "${self'.packages.base16-schemes}/share/themes/rose-pine.yaml";
+    polarity = "dark";
+    fonts = {
+      sansSerif = {
+        package = pkgs.work-sans;
+        name = "Work Sans";
+      };
+      serif = config.stylix.fonts.sansSerif; # Set serif font to the same as the sans-serif
+      monospace = {
+        package = self'.packages.liga-sf-mono-nerd-font;
+        name = "Liga SFMono Nerd Font";
+      };
+      emoji = {
+        package = self'.packages.apple-emoji;
+        name = "Apple Color Emoji";
+      };
 
-    sizes = {
-      applications = 10;
-      desktop = 10;
-      popups = 10;
-      terminal = 12;
+      sizes = {
+        applications = 10;
+        desktop = 10;
+        popups = 10;
+        terminal = 12;
+      };
     };
+    image =
+      let
+        wallpaper = pkgs.fetchurl {
+          url = "https://w.wallhaven.cc/full/8x/wallhaven-8xe57j.jpg";
+          sha256 = "036ld3x0y0frkjcspb1qfsjnqnxl5kq9nh2f0wrp54mlx7rmcmip";
+        };
+      in
+      pkgs.runCommand "output.png" { }
+        "${lib.getExe pkgs.lutgen} apply ${wallpaper} -o $out -- ${builtins.concatStringsSep " " config.lib.stylix.colors.toList}";
   };
 in
 {
@@ -39,20 +50,6 @@ in
 
       stylix = {
         enable = true;
-        inherit base16Scheme polarity;
-
-        image =
-          let
-            wallpaper = pkgs.fetchurl {
-              url = "https://w.wallhaven.cc/full/8x/wallhaven-8xe57j.jpg";
-              sha256 = "036ld3x0y0frkjcspb1qfsjnqnxl5kq9nh2f0wrp54mlx7rmcmip";
-            };
-          in
-          pkgs.runCommand "output.png" { }
-            "${lib.getExe pkgs.lutgen} apply ${wallpaper} -o $out -- ${builtins.concatStringsSep " " config.lib.stylix.colors.toList}";
-
-        fonts = fonts pkgs config;
-
         cursor = {
           package =
             with config.lib.stylix.colors.withHashtag;
@@ -70,7 +67,8 @@ in
           popups = 0.75;
           terminal = 0.75;
         };
-      };
+      }
+      // (sharedStylixConfig config pkgs);
     };
 
   home-manager =
@@ -91,21 +89,29 @@ in
 
       stylix = {
         enable = true;
-        inherit base16Scheme polarity;
-        fonts = fonts pkgs config;
-      };
+      }
+      // (sharedStylixConfig config pkgs);
 
-      system.defaults = {
-        NSGlobalDomain = {
-          AppleInterfaceStyle = "Dark";
-          AppleInterfaceStyleSwitchesAutomatically = false;
-          NSStatusItemSpacing = 8; # default=12
-          NSStatusItemSelectionPadding = 6; # default=6
-          _HIHideMenuBar = false;
-          NSAutomaticWindowAnimationsEnabled = false;
+      system = {
+        activationScripts.postActivation.text =
+          # bash
+          ''
+            printf >&2 '%b' 'Setting \033[0;35mStylix\e[0m wallpaper...\n'
+            osascript -e 'tell application "System Events" to tell every desktop to set picture to "${config.stylix.image}" as POSIX file'
+          '';
+
+        defaults = {
+          NSGlobalDomain = {
+            AppleInterfaceStyle = "Dark";
+            AppleInterfaceStyleSwitchesAutomatically = false;
+            NSStatusItemSpacing = 8; # default=12
+            NSStatusItemSelectionPadding = 6; # default=6
+            _HIHideMenuBar = false;
+            NSAutomaticWindowAnimationsEnabled = false;
+          };
+          spaces.spans-displays = true;
+          dock.expose-group-apps = true;
         };
-        spaces.spans-displays = true;
-        dock.expose-group-apps = true;
       };
     };
 }
