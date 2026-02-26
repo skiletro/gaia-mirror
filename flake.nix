39434a1255ffac503782d8178b445414fe772a1b
jamie <git@skilet.ro>
2026-02-26 14:15:45 +0000

    feat(flake): add nix-community cachix substituter

--- flake.nix
+++ flake.nix
@@ -112,6 +112,7 @@
 
   nixConfig = {
     extra-trusted-substituters = [
+      "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
       "https://attic.xuyh0120.win/lantian"
       "https://helix.cachix.org"
@@ -120,6 +121,7 @@
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
       "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 }
