66fede8abbf743d16d86f48cbda46f0184835f1f
jamie <git@skilet.ro>
2026-02-26 17:19:29 +0000

    revert: "chore: remove unused inputs"
    This reverts commit 83b9ddcc84126a0dfe5e6eb08ba3521af6216d6b.

--- flake.nix
+++ flake.nix
@@ -14,6 +14,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    dms-plugin-registry = {
+      url = "github:AvengeMedia/dms-plugin-registry";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     dms.url = "github:AvengeMedia/DankMaterialShell";
 
     drasl.url = "github:unmojang/drasl";
