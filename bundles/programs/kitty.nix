3e1b7602271767d904495586779ad49393101347
jamie <git@skilet.ro>
2026-03-06 16:59:14 +0000

    feat(kitty): update config

--- bundles/programs/kitty.nix
+++ bundles/programs/kitty.nix
@@ -10,6 +10,9 @@ bundleLib.mkEnableModule [ "gaia" "programs" "kitty" ] {
         wayland_title_bar = "background";
         dynamic_background_opacity = true;
         window_padding_width = 6;
+        cursor_shape = "beam";
+        cursor_trail = 1;
+        macos_quit_when_last_window_closed = true;
       };
     };
   };
