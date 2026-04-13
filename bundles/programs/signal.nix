51ceb9cb1791fd8dd42781ef03818ee0f96e7a90
jamie <git@skilet.ro>
2026-04-13 23:48:22 +0100

    feat(signal): increase screenshare fps from 5 to 15

--- bundles/programs/signal.nix
+++ bundles/programs/signal.nix
@@ -10,7 +10,28 @@ bundleLib.mkEnableModule [ "gaia" "programs" "signal" ] {
   home-manager =
     { pkgs, ... }:
     {
-      home.packages = [ pkgs.signal-desktop ];
+      home.packages = [
+        (pkgs.signal-desktop.overrideAttrs (oldAttrs: {
+          patches = oldAttrs.patches ++ [
+            (pkgs.writeText "more-fps.patch" ''
+              diff --git a/ts/calling/constants.std.ts b/ts/calling/constants.std.ts
+              index df863d7a3..29d84b964 100644
+              --- a/ts/calling/constants.std.ts
+              +++ b/ts/calling/constants.std.ts
+              @@ -14,7 +14,7 @@ export const REQUESTED_GROUP_VIDEO_HEIGHT = 480;
+               export const REQUESTED_SCREEN_SHARE_WIDTH = 2880;
+               export const REQUESTED_SCREEN_SHARE_HEIGHT = 1800;
+               // 15fps is much nicer but takes up a lot more CPU.
+              -export const REQUESTED_SCREEN_SHARE_FRAMERATE = 5;
+              +export const REQUESTED_SCREEN_SHARE_FRAMERATE = 15;
+               
+               export const MAX_FRAME_WIDTH = 2880;
+               export const MAX_FRAME_HEIGHT = 1800;
+
+            '')
+          ];
+        }))
+      ];
     };
 
 }
