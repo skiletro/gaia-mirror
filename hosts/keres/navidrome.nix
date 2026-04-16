9b2b63c42de2f9dac8c46c0e6e203546b31eb154
jamie <git@skilet.ro>
2026-04-16 17:50:16 +0100

    feat(navidrome): update settings and state dir

--- hosts/keres/navidrome.nix
+++ hosts/keres/navidrome.nix
@@ -1,11 +1,12 @@
-{ self', lib, ... }:
+{ self', ... }:
 {
   nixos =
-    { pkgs, ... }:
     let
       subdomain = "navidrome";
       domain = "warm.vodka";
       port = 4533;
+
+      rootFolder = "/srv/navidrome";
     in
     {
       services.navidrome = {
@@ -13,29 +14,14 @@
         settings = {
           Address = "127.0.0.1";
           Port = port;
-          MusicFolder = "/mnt/music";
-          EnableSharing = true;
-          UIWelcomeMessage = "Hi! Please use a dedicated Desktop/Mobile app instead of this frontend.";
-          Plugins.Enabled = true;
-          Agents = "deezer,audiomuseai";
+          MusicFolder = "${rootFolder}/music";
+          DataFolder = "${rootFolder}/data";
+          CacheFolder = "${rootFolder}/cache";
+          DefaultTheme = "Spotify-ish";
+          EnableSharing = false;
+          EnableStarRating = false;
         };
       };
-
-      systemd.services.navidrome.serviceConfig.ExecStartPre =
-        let
-          audiomuseai = pkgs.fetchurl {
-            url = "https://github.com/NeptuneHub/AudioMuse-AI-NV-plugin/releases/download/v7/audiomuseai.ndp";
-            sha256 = "1y23vb5kip1gzd0i1c7lr4dwamyn8irzql5y0zhz17gbqf1w5dzs";
-          };
-        in
-        [
-          "${lib.getExe' pkgs.coreutils "mkdir"} -p /var/lib/navidrome/plugins"
-          "${lib.getExe' pkgs.coreutils "cp"} -f ${audiomuseai} /var/lib/navidrome/plugins/audiomuseai.ndp"
-        ];
-
-      systemd.tmpfiles.rules = [
-        "d /mnt/music 0770 navidrome navidrome -"
-      ];
 
       services.caddy.virtualHosts."${subdomain}.${domain}".extraConfig = ''
         reverse_proxy :${toString port}
