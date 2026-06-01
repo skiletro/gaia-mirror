21da6cfa28aebcd4f6f5516510edf6bade224b99
jamie <git@skilet.ro>
2026-06-01 19:45:44 +0100

    refactor(steam): move module logic into bundle

--- bundles/programs/steam.nix
+++ bundles/programs/steam.nix
@@ -1,6 +1,7 @@
 {
   bundleLib,
   self',
+  inputs,
   inputs',
   lib,
   ...
@@ -9,21 +10,15 @@ bundleLib.mkEnableModule [ "gaia" "programs" "steam" ] {
 
   gaia.autoStart = [ "steam -silent -console" ];
 
-  nixos =
-    { pkgs, ... }:
+  home-manager =
+    { osConfig, pkgs, ... }:
     {
+      imports = [ inputs.steam-config-nix.homeModules.default ];
 
-      programs.steam = rec {
-        enable = true;
-        extraCompatPackages = [
-          self'.packages.proton-cachyos_x86_64_v3
-          inputs'.nixpkgs-xr.packages.proton-ge-rtsp-bin
-          pkgs.steam-play-none # Allows you to run a game without Proton if it is otherwise forced.
-        ];
+      programs.steam.config = rec {
+        enable = lib.mkIf osConfig.programs.steam.enable true;
+        closeSteam = true;
         defaultCompatTool = "Proton-CachyOS x86-64-v3";
-        extest.enable = true;
-        protontricks.enable = true;
-        gamescopeSession.enable = true;
         apps =
           let
             fpsCap = toString 174;
@@ -116,6 +111,45 @@ bundleLib.mkEnableModule [ "gaia" "programs" "steam" ] {
       };
     };
 
-  darwin.homebrew.casks = [ "steam" ];
+  nixos =
+    { pkgs, ... }:
+    {
 
+      programs.steam = {
+        enable = true;
+        package = pkgs.steam.override {
+          extraProfile = ''
+            export DXVK_HUD=compiler
+            export PROTON_ENABLE_WAYLAND=1
+            export PROTON_ENABLE_HDR=1
+            export PROTON_FSR4_UPGRADE=1
+            export PRESSURE_VESSEL_IMPORT_OPENXR_1_RUNTIMES=1
+            unset TZ
+          '';
+        };
+        extraCompatPackages = [
+          self'.packages.proton-cachyos_x86_64_v3
+          inputs'.nixpkgs-xr.packages.proton-ge-rtsp-bin
+          pkgs.steam-play-none # Allows you to run a game without Proton if it is otherwise forced.
+        ];
+        extest.enable = true;
+        protontricks.enable = true;
+        gamescopeSession.enable = true;
+      };
+
+      environment.systemPackages = with pkgs; [
+        # keep-sorted start
+        sgdboop
+        steamtinkerlaunch
+        # keep-sorted end
+      ];
+
+      boot.kernel.sysctl."vm.max_map_count" = 2147483642;
+    };
+
+  darwin = {
+    config.homebrew.casks = [ "steam" ];
+
+    options.programs.steam.enable = lib.mkOption { default = false; };
+  };
 }
