c9c7b36153064a16b4ac6544e3279ffa813665d1
jamie <git@skilet.ro>
2026-06-17 20:44:41 +0100

    refactor(treewide): small changes for tidiness

--- hosts/iso/installer.nix
+++ hosts/iso/installer.nix
@@ -1,16 +1,5 @@
 { inputs', ... }:
 {
-  gaia = {
-    programs = {
-      # keep-sorted start
-      git.enable = true;
-      nu.enable = true;
-      term-utils.enable = true;
-      # keep-sorted end
-    };
-    state.system = "26.05";
-  };
-
   nixos =
     { modulesPath, ... }:
     {
